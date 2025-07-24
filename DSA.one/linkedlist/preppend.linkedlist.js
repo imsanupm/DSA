873907ae2a469d5b02a2185bd7defa2e8f1cee34
iamsanupm <sanukrishna23@gmail.com>
2025-07-24 18:03:09 +0530

    after learn linkedlist creation

---
+++ DSA.one/linkedlist/preppend.linkedlist.js
@@ -0,0 +1,50 @@
+//create linkedlist and prepend 
+
+class nod{
+    constructor(value){
+        this.value = value;
+        this.next = null;
+    }
+}
+
+class linkedlist{
+    constructor(){
+        this.head = null;
+        this.size = 0;
+    }
+    length(){
+        return this.size===0
+    }
+    
+    append(value){
+      let node = new nod(value)
+          if(this.length()){
+              this.head = node;
+          }else{
+             node.next = this.head;
+              this.head = node;
+          }
+          this.size++
+      
+    }
+    print(){
+      let current = this.head;
+      let val = "";
+      while(current){
+          val+=current.value;
+          console.log(val);
+          current = current.next
+      }
+    }
+}
+
+const  list = new linkedlist();
+list.append(5);
+list.print()
+
+
+
+
+
+
+
