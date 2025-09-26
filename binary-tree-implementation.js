89e9f5c6398237ce98a6bc6085e5083cc8ff3886
iamsanupm <sanukrishna23@gmail.com>
2025-09-26 13:24:10 +0530

    binary-search implementation

---
+++ binary-tree-implementation.js
@@ -0,0 +1,54 @@
+class Node{
+    constructor(value) {
+        this.data = value;
+        this.left = null;
+        this.right = null;
+    }
+};
+
+class BinaryTree{
+    constructor(){
+        this.root = null;
+    };
+
+    insert(value){
+        const newNode = new Node(value);
+
+
+        if(!this.root){
+            this.root = newNode;
+            return;
+        }
+
+        const queue = [this.root];
+
+        while(queue.length){
+            const node = queue.shift();
+
+            if(!node.left){
+                node.left = newNode;
+                return;
+            } else {
+                queue.push(node.left);
+            }
+
+            if(!node.right){
+                node.right = newNode;
+                return;  
+            } else {
+                queue.push(node.right);
+            }
+        }
+    }
+}
+
+
+const bt = new BinaryTree();
+
+bt.insert(3);
+bt.insert(2);
+bt.insert(23);
+bt.insert(99);
+bt.insert(323);
+
+console.log(JSON.stringify(bt.root, null, 2));
